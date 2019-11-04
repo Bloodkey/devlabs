5256487f433b942e680dfd4fe174739c1c9b33d2
Eugene <bloodkeyss@gmail.com>
2019-11-04 13:48:22 +0200

    lab3: server.log fixed

--- lab3/main/views.py
+++ lab3/main/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import platform
-import os
 from datetime import datetime
+
 
 
 def main(request):
--- lab3/monitoring.py
+++ lab3/monitoring.py
@@ -3,7 +3,6 @@ import json
 import logging
 import time
 import threading
-import chardet
 
 logging.basicConfig(
     filename="server.log",
@@ -12,27 +11,24 @@ logging.basicConfig(
     format='{levelname} {asctime} {name} : {message}',
     style='{'
 )
-
 log = logging.getLogger(__name__)
-
 def run():
     main("http://localhost:8000/health")
-
 def main(url):
     try:
-        response = requests.get(url)
-        response.raise_for_status()
-        data = json.loads(response.content.decode(chardet.detect(response.content)["encoding"]))
-        logging.info("Server date: %s", data['date'])
+        r = requests.get(url)
+        r.raise_for_status()
+        data = json.loads(r.content)
+        logging.info("Server available, server date: %s", data['date'])
         logging.info("Monitoring page: : %s", data['current_page'])
         logging.info("Server info: %s", data['server_info'])
         logging.info("Client info: %s", data['client_info'])
     except Exception as e:
         logging.error("Exception: %s", e)
 
-    while True:
+    while 1 == 1:
         time.sleep(60)
         run()
 
 if __name__ == '__main__':
-    run() 
+    run()
